6c749ad83793f0e1e501faddf45089509b8584fe
JorgeMiguelDelgado <41923835+JorgeMiguelDelgado@users.noreply.github.com>
2022-06-07 23:10:26 -0400

    interesante

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Indecision/>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <!--<Counter :start="4"/>-->
+
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
+import Indecision from './components/Indecision.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //Counter,
+    Indecision,
   }
+
+  
 }
 </script>
 
---
+++ src/components/Counter.vue
@@ -0,0 +1,81 @@
+<template>
+    <h2>{{ customTitle }}</h2>
+    
+    <p>{{counter }} = {{squarecounter}}</p>
+    
+    <div>
+        <button @click="incrementar">+1</button>
+        <button @click="decrementar">-1</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    //Propiedades
+    props:{
+        title: String,
+        start: {
+            type: Number,
+            default: 3,
+            validator(value){
+                return value >=0
+            }
+            
+        },
+    },
+    data()
+    {
+        return{
+            counter: this.start
+        }
+    },
+    methods:{
+        getSquarevalue()
+        {
+            return this.counter * this.counter
+        },
+        incrementar()
+        {
+            
+            this.counter++
+            
+        },
+        decrementar()
+        {
+            this.counter--
+        }
+    },
+    computed:{
+        squarecounter()
+        {
+            return this.counter * this.counter
+        },
+        customTitle()
+        {
+            return this.title || 'Counter'
+        }
+        
+    }
+}
+</script>
+
+<style>
+    button
+    {
+        margin: 0 5px;
+        background-color: #64bb87;
+        border-radius: 5px;
+        color: white;
+        cursor: pointer;
+        border: 1px solid white;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+        
+    }
+    button:hover
+    {
+        background-color: #5aa67b;
+        transition: 0.3s ease-in-out;
+    }
+</style>
